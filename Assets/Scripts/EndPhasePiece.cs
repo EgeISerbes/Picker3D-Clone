2c532999c3ac3c3e2aad285517beeb25c9de504f
egeserbes <egeserbes55@gmail.com>
2022-09-13 06:23:30 +0200

    added endphase scripts and prefabs

---
+++ Assets/Scripts/EndPhasePiece.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndPhasePiece : MonoBehaviour
+{
+    [SerializeField] TextMeshPro _text;
+    [SerializeField] private int _val;
+    private void Awake()
+    {
+        _text.SetText(_val.ToString());
+    }
+}
